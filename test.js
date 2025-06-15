6a59a5cb1983a5ce9ec0f1cddebf4b979fc2e82a
IrenOkey <giicastle7@gmail.com>
2025-06-15 11:31:04 +0700

    third

---
+++ test.js
@@ -0,0 +1,5 @@
+document.getElementById("blogButton").addEventListener("click", function (e) {
+    e.preventDefault(); // mencegah aksi default <a>
+    window.location.href = "blog.html"; // pindah ke halaman blog.html
+  });
+  
